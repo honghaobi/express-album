5a5a8c09a87524acb2735cc311774d7d448fd2e7
henrybi <henrybi@u.washington.edu>
2016-03-31 14:07:56 -0700

    finished update!

--- album-express-app/routes/albums.js
+++ album-express-app/routes/albums.js
@@ -8,7 +8,7 @@ function Albums() {
 
 
 router.get('/albums', function(req, res, next) {
-  Albums().select().then(function (records) {
+  Albums().select().orderBy('id','asc').then(function (records) {
     res.render('albums/index', {allAlbums: records});
   })
 });
@@ -34,5 +34,11 @@ router.get('/albums/:id/edit', function(req, res, next) {
     res.render('albums/edit', {theAlbum: record});
   })
 })
+
+router.post('/albums/:id/edit', function(req, res, next) {
+  Albums().where({name: req.body.album_name}).update({ artist: req.body.album_artist, name: req.body.album_name, genre: req.body.album_genre, stars: req.body.album_stars, explicit: req.body.album_explicit}).then(function () {
+    res.redirect('/albums');
+  });
+});
 
 module.exports = router;
