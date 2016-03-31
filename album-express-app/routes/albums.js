5ee7eb90d55b00da077c8b047a8f7b03c931094e
henrybi <henrybi@u.washington.edu>
2016-03-31 11:16:12 -0700

    populated to database

--- album-express-app/routes/albums.js
+++ album-express-app/routes/albums.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+
+function Albums() {
+  return knex('albums');
+}
+
 
 router.get('/albums', function(req, res, next) {
   res.render('albums/index');
@@ -10,7 +16,9 @@ router.get('/albums/new', function(req, res, next) {
 });
 
 router.post('/albums', function(req, res, next) {
-  res.redirect('/albums');
-})
+  Albums().insert({ artist: req.body.album_artist, name: req.body.album_name, genre: req.body.album_genre, stars: req.body.album_stars, explicit: req.body.album_explicit}).then(function () {
+    res.redirect('/albums');
+  });
+});
 
 module.exports = router;
